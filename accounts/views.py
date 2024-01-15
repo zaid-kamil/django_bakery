d4564f26f91c7ecb6a5665188c52a212d4351993
zaidkamil <xaidmetamorphos@gmail.com>
2024-01-15 18:34:30 +0530

    toast, slider, data fetch

--- accounts/views.py
+++ accounts/views.py
@@ -42,4 +42,4 @@ def register_view(request):
 def logout_view(request):
     logout(request)
     messages.success(request, "logout successful")
-    return redirect('login')
+    return redirect('home')
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from inventory.models import Collection
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', context={
+        'collections': Collection.objects.all()[:4] # get first 4 collections
+    })
 
 
 # about func -> about.html
